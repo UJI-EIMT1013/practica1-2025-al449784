4562cd843112e3960c86cfb1ca00cad4cf689348
Pablo <al4497784@uji.es>
2025-10-02 14:22:49 +0200

    Ejercicio 2

--- Practica1.java
+++ Practica1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Practica1 {
 
     //EJERCICIO 1
-    public static Set<Integer> multiplos(Iterator<Integer> it) {
+    public static Set<Integer> multiplos (Iterator<Integer> it) {
         List<Integer> lista = new ArrayList<>();
 
         // Convertimos el iterador en lista
@@ -33,4 +33,34 @@ public class Practica1 {
 
         return resultado;
     }
-}
+
+    //EJERCICIO2
+    public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
+        Set<Integer> todos = new HashSet<>();
+        todos.addAll(cuadrados);
+        todos.addAll(noCuadrados);
+
+        Set<Integer> posiblesCuadrados = new HashSet<>();
+        for (int n : todos) {
+            posiblesCuadrados.add(n * n);
+        }
+
+        Set<Integer> nuevosCuadrados = new HashSet<>();
+        Set<Integer> nuevosNoCuadrados = new HashSet<>();
+
+        for (int n : todos) {
+            if (posiblesCuadrados.contains(n)) {
+                nuevosCuadrados.add(n);
+            } else {
+                nuevosNoCuadrados.add(n);
+            }
+        }
+
+        cuadrados.clear();
+        cuadrados.addAll(nuevosCuadrados);
+
+        noCuadrados.clear();
+        noCuadrados.addAll(nuevosNoCuadrados);
+    }
+
+    }
