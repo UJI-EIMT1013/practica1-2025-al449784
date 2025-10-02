55d8d97961b65a1cd701b4413375c8a5f33063c4
Pablo <al4497784@uji.es>
2025-10-02 14:03:47 +0200

    Ejercicio 1

--- Practica1.java
+++ Practica1.java
@@ -5,29 +5,32 @@ import java.util.*;
 public class Practica1 {
 
     //EJERCICIO 1
-    public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+    public static Set<Integer> multiplos(Iterator<Integer> it) {
+        List<Integer> lista = new ArrayList<>();
+
+        // Convertimos el iterador en lista
+        while (it.hasNext()) {
+            int num = it.next();
+            if (num != 0) { // Ignorar ceros
+                lista.add(num);
+            }
+        }
+
+        Set<Integer> resultado = new HashSet<>();
+
+        // Comparar cada par de elementos
+        for (int i = 0; i < lista.size(); i++) {
+            for (int j = 0; j < lista.size(); j++) {
+                if (i != j) { // No comparar consigo mismo
+                    int a = lista.get(i);
+                    int b = lista.get(j);
+                    if (a % b == 0) {
+                        resultado.add(a);
+                    }
+                }
+            }
+        }
+
+        return resultado;
     }
-
-    //EJERCICIO2
-    public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
-
-    }
-
-    //EJERCICIO 3
-    public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
-        //TODO
-        return null;
-    }
-
-    //EJERCICIO 4
-    public static<T> Collection<Set<T>> coverageSet2 (Set<T> u,ArrayList<Set<T>> col) {
-        //TODO
-        return null;
-    }
-
-
-
 }
